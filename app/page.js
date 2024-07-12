0ae7ddc30751c0b8597c75a32f4782e92c86b36d
techinezz <y.martinez.tech@gmail.com>
2024-07-12 02:56:12 -0400

    3d model update

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import styles from "./page.module.css";
+import ThreeScene from "../app/components/ThreeScene";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <div className={styles.action}>
         <button className={styles.actionButton}>Ready? Reserve for Spot</button>
         </div>
+        <ThreeScene />
       </div>
     </main>
   );
