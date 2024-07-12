d39f05e240e099e59bd6302e36b858cc307d2159
Marilyn Jin <marilynj468@gmail.com>
2024-07-12 09:53:52 -0400

    styling changes

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <h1>Sip & Play</h1>
+        <h1 className={styles.title}>Sip & Play 🧋</h1>
         <p className={styles.description}>
           Classic eats, bubble tea & beer served in <br className={styles.hidden} /> an old-school, brick-lined cafe boasting 300+ board games.
         </p>
